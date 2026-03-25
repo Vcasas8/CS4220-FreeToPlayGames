03770d5fb61a9e1857fd90bbda0d43e883aa9c79
Austin Wong <95609378+AYWong0305@users.noreply.github.com>
2026-03-25 15:56:52 -0700

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,85 @@ Objectives for app.js
     + Gets and displays information for the selected item from the API
         - NOT raw JSON
  */
+
+import inquirer from "inquirer";
+import { searchGamesByKeyword, getGameDetailsById } from "./api.js";
+import { addToSearchHistory, displaySearchHistory } from "./history.js";
+
+// Search Flow
+export async function searchGames(keyword) {
+    try {
+        const results = await searchGamesByKeyword(keyword);
+
+        if (results.length === 0) {
+            console.log("No games found.\n");
+            return;
+        }
+
+        // Save keyword
+        await addToSearchHistory(keyword);
+
+        // Build choices
+        const choices = [
+            { name: "Exit", value: null },
+            ...results.map(game => ({
+                name: `${game.title} (${game.gnere}, ${game.platform})`,
+                value: game.id
+            }))
+        ];
+
+        const answer = await inquirer.prompt([
+            {
+                type: "list",
+                name: "gameId",
+                message: "Select a game: ",
+                choices
+            }
+        ]);
+
+        if (!answer.gameId) return;
+
+        const details = await getGameDetailsById(answer.gameId);
+
+        console.log("\n=== Game Details ===");
+        console.log(`Title: ${details.title}`);
+        console.log(`Genre: ${details.genre}`);
+        console.log(`Platform: ${details.platform}`);
+        console.log(`Release Date: ${details.releaseDate}`);
+        console.log(`Description: ${details.description}\n`);
+
+    } catch (err) {
+        console.log("Search failed: ", err.message);
+    }
+
+}
+
+// History flow
+export async function showHistory() {
+    try {
+        const history = await displaySearchHistory();
+
+        if (history.length === 0) {
+            console.log("No search history.\n");
+            return;
+        }
+
+        const choices = ["Exit", ...history];
+
+        const answer = await inquirer.prompt ([
+            {
+                type: "list",
+                name: "keyword",
+                message: "Search History: ",
+                choices
+            }
+        ]);
+
+        if (answer.keyword === "Exit") return;
+
+        await searchGames(answer.keyword);
+
+    } catch (err) {
+        console.error("History error: ", err.message);
+    }
+}
